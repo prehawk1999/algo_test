6ead7f963f08559377aad834267de8b59235d229
prehawk1999 <prehawk@gmail.com>
2014-04-29 23:15:44 +0800

    Add file
    Evalue Expression

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lboost_system -lboost_timer
+LIBS := -lboost_system -lboost_timer -l"E:/boost_1_55_0/stage/lib/libboost_system-mgw47-mt-1_55.a" -l"E:/boost_1_55_0/stage/lib/libboost_timer-mgw47-mt-1_55.a"
 
