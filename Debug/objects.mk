723812a8f77abebd0c538f1df48c724e60e62d8d
prehawk1999 <prehawk@gmail.com>
2014-05-11 16:30:18 +0800

    add things

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lboost_system -lboost_timer -l"E:/boost_1_55_0/stage/lib/libboost_system-mgw47-mt-1_55.a" -l"E:/boost_1_55_0/stage/lib/libboost_timer-mgw47-mt-1_55.a"
-
